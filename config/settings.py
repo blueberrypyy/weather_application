3e17939f32ead523172f1043c441eda2e796e580
Jay <blueberry.py@protonmail.com>
2024-07-30 11:07:19 -0600

    Fixed map view

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = env.bool('DEBUG', default=False)
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
